f75b142bce9b055f80a7d341774532c5589e312d
claudine Kuradusenge <claudinekuradusenge2@gmail.com>
2020-10-23 12:06:25 +0200

    add the whole work

---
+++ mail-script.php
@@ -0,0 +1,19 @@
+<?php
+
+    $myAwardSpaceEmail = "kaliclaudine@yahoo.com";
+    $myPersonalEmail = "kaliclaudine@yahoo.com";
+    
+    if(isset($_POST['submit'])) {
+        $subject = $_POST['subject'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $headers = "From: Contact Form <" . $myAwardSpaceEmail . ">" . "\r\n";
+        $headers .= "Reply-To: " . $name . " <" . $email .">" . "\r\n";
+        
+        echo 'Your message was sent successfully!';
+        mail($myPersonalEmail, $subject, $message, $headers);
+    } else {
+        echo 'An error has occurred!';
+    }
+?>
